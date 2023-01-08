aed447982233ec41091ae96209a58d420dfcf0d7
MelnikovEI <MelnikovEI@ya.ru>
2023-01-08 08:54:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Comics publisher
+
+Script posts one random comics from [xkcd.com](https://xkcd.com/) to VK community.
+
+### Prerequisites
+1. Create a [community in VK](https://vk.com/groups?tab=admin) and [get your GROUP_ID](https://regvk.com/id/)
+2. Create an [app in VK](https://vk.com/apps?act=manage)
+   - Then press "manage" on your app and you'll see the "client_id" at the end of URL:
+     https://vk.com/editapp?id= `11111111`
+3. Get your access_token, using [Implicit Flow](https://vk.com/dev/implicit_flow_user) procedure.
+   
+   Do not use parameter redirect_uri in request.
+   
+   Parameter scope: scope=photos,groups,wall,offline.
+
+4. Create environment variables in "your_project_folder\\ .env" file:
+- VK_GROUP_ID= group_id from p.1 above.
+- CLIENT_ID= client_id from p.2 above.
+- ACCESS_TOKEN= access_token from p.3 above.
+   
+5. Python3 should be already installed.
+
+   Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+-> packages, listed in requirements, should be successfully installed. 
+
+### Usage
+
+Run the script:
+
+```
+python main.py
+```
+
+## Authors
+* [Evgeny Melnikov](https://github.com/MelnikovEI)
+## Acknowledgments
+* Inspired by [Devman](https://dvmn.org/)
---
+++ main.py
@@ -0,0 +1,108 @@
+import os
+import random
+import urllib
+from pathlib import Path
+import requests
+from environs import Env
+
+VK_API_VERSION = '5.131'
+
+
+def load_image(url, file_name, params=None):
+    Path(Path.cwd() / 'images').mkdir(parents=True, exist_ok=True)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with open(Path.cwd() / 'images' / file_name, 'wb') as file_to_save:
+        file_to_save.write(response.content)
+
+
+def get_ext(url: str) -> str:
+    split_url = urllib.parse.urlsplit(url)
+    split_path = os.path.split(split_url.path)
+    split_file_name = os.path.splitext(split_path[1])
+    return split_file_name[1]
+
+
+def get_last_comics_numb():
+    last_comics_url = "https://xkcd.com/info.0.json"
+    xkcd_response = requests.get(last_comics_url)
+    xkcd_response.raise_for_status()
+    comics_item = xkcd_response.json()
+    return comics_item['num']
+
+
+def load_rnd_comics():
+    numb = random.randint(1, get_last_comics_numb())
+    comics_url = f"https://xkcd.com/{numb}/info.0.json"
+    xkcd_response = requests.get(comics_url)
+    xkcd_response.raise_for_status()
+    comics_item = xkcd_response.json()
+    img_link = comics_item['img']
+    comics_comment = comics_item['alt']
+    if img_link:
+        img_file_mane = f'comics_{numb}{get_ext(img_link)}'
+        load_image(img_link, img_file_mane)
+        return img_file_mane, comics_comment
+
+
+def comics_posting(params, img_file_name, comics_comment):
+    params = params
+    # Получаем адрес сервера для загрузки фото
+    url = 'https://api.vk.com/method/photos.getWallUploadServer'
+    vk_response = requests.get(url, params=params)
+    vk_response.raise_for_status()
+    upload_url = vk_response.json()['response']['upload_url']
+
+    # Отправляем фото на сервер
+    with open(os.path.join('images', img_file_name), 'rb') as file:
+        files = {
+            'photo': file,
+        }
+        vk_response = requests.post(upload_url, files=files)
+        vk_response.raise_for_status()
+        uploaded_photo_params = vk_response.json()
+
+    # Сохраняем фото на стене
+    params.update({
+        'server': uploaded_photo_params['server'],
+        'photo': uploaded_photo_params['photo'],
+        'hash': uploaded_photo_params['hash'],
+    })
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    vk_response = requests.post(url, params=params)
+    vk_response.raise_for_status()
+    uploaded_photo = vk_response.json()
+
+    # Публикуем комикс в группе
+    photo_id = uploaded_photo['response'][0]['id']
+    owner_id = uploaded_photo['response'][0]['owner_id']
+    url = 'https://api.vk.com/method/wall.post'
+    params.update({
+        'attachments': f"photo{owner_id}_{photo_id}",
+        'message': comics_comment,
+    })
+    vk_response = requests.post(url, params=params)
+    vk_response.raise_for_status()
+
+
+def main():
+    env = Env()
+    env.read_env()
+    params = {
+        'access_token': env('ACCESS_TOKEN'),
+        'v': VK_API_VERSION,
+        'group_id': env('VK_GROUP_ID'),
+        'owner_id': f"-{env('VK_GROUP_ID')}",
+        'from_group': '1',
+    }
+
+    # Скачиваем комикс
+    img_file_name, comics_comment = load_rnd_comics()
+    # Публикуем комикс
+    comics_posting(params, img_file_name, comics_comment)
+    # Удаляем скачанный файл
+    os.remove(os.path.join('images', img_file_name))
+
+
+if __name__ == '__main__':
+    main()
